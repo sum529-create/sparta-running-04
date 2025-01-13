433d1c2ebd33f089abbad410df4e5e617b56b6ba
노수민 <nosumin29@gmail.com>
2025-01-13 18:04:08 +0900

    feat: 6번 문제 해결

--- 06.js
+++ 06.js
@@ -10,7 +10,12 @@
  * @param {Array} users
  * @returns {Array}
  */
-function transformUsers(users) {}
+function transformUsers(users) {
+  return users.map((e) => ({
+    fullName: e.firstName && e.lastName ? e.firstName + " " + e.lastName : "",
+    isAdult: e.age >= 20,
+  }));
+}
 
 // export 수정 불가
 export { transformUsers };
